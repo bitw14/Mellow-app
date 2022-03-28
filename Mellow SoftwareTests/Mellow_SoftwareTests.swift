e834de837bb1ed6b100a91beaeda08317fed4915
Abdul Waheed <akabdul14@gmail.com>
2022-03-28 19:30:13 +0500

    Initial Commit

---
+++ Mellow SoftwareTests/Mellow_SoftwareTests.swift
@@ -0,0 +1,33 @@
+//
+//  Mellow_SoftwareTests.swift
+//  Mellow SoftwareTests
+//
+//  Created by Abdul Waheed on 28/03/2022.
+//
+
+import XCTest
+@testable import Mellow_Software
+
+class Mellow_SoftwareTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Mellow SoftwareUITests/Mellow_SoftwareUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Mellow_SoftwareUITestsLaunchTests.swift
+//  Mellow SoftwareUITests
+//
+//  Created by Abdul Waheed on 28/03/2022.
+//
+
+import XCTest
+
+class Mellow_SoftwareUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
